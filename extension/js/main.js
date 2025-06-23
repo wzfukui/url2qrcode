2eb8d646c0cf9e8154b0f6f8251af54bf73bcde2
fukui <146505187+wzfukui@users.noreply.github.com>
2025-06-23 14:35:54 +0800

    Enhance popup UI with Bootstrap and add copy functionality

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,21 +1,27 @@
-// xkwy, 2019/3/14
 function refreshQR(text) {
-    console.log("this.value=[" + this.value + "], text=[" + text + "]");
+    const value = qrtext_url.value || text || '';
     qrcode.stringToBytes = qrcode.stringToBytesFuncs['UTF-8'];
-    var qr = qrcode(0, 'L');
-    qr.addData(this.value || text);
+    const qr = qrcode(0, 'L');
+    qr.addData(value);
     qr.make();
     qrimg.innerHTML = qr.createSvgTag(4);
 }
 
-qrtext_url.addEventListener("input", refreshQR);
+qrtext_url.addEventListener('input', () => refreshQR());
 
-chrome.tabs.query({'active': true}, function(tabs){
-    var url = tabs[0].url;
+copy_btn.addEventListener('click', () => {
+    navigator.clipboard.writeText(qrtext_url.value).then(() => {
+        copy_btn.innerText = 'Copied!';
+        setTimeout(() => { copy_btn.innerText = 'Copy'; }, 2000);
+    });
+});
+
+chrome.tabs.query({ active: true }, function(tabs) {
+    const url = tabs[0].url;
     qrtext_url.value = url;
     qrtext_url.select();
     refreshQR(url);
-    
-    var title = tabs[0].title;
+
+    const title = tabs[0].title;
     qrtext_title.value = title;
 });
