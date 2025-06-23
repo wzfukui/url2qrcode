315f8ecfd25a0bb51c2283700c27bc1d342cc863
fukui <146505187+wzfukui@users.noreply.github.com>
2025-06-23 14:21:32 +0800

    refactor: migrate to MV3 and reorganize

---
+++ extension/js/background.js
@@ -0,0 +1,16 @@
+function genericOnClick(info, tab) {
+    console.log('genericOnClick hass been called');
+    console.log(info);
+    console.log(tab);
+    chrome.tabs.sendMessage(tab.id, info);
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'createQrcode',
+        title: '生成二维码',
+        contexts: ['all']
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(genericOnClick);
---
+++ extension/js/content.js
@@ -0,0 +1,6 @@
+
+console.log("content loaded!");
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+    console.log(request);
+    console.log(request.selectionText);
+});
